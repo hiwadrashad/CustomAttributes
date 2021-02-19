e8229e0fa3884ece5a5fe71192e111e689dbd78d
hiwad rashad <hiwadrashad112@hotmail.com>
2021-02-19 13:23:26 +0100

    Add project files.

---
+++ CustomAttributes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class CustomAttribtie : Attribute
+{ 
+   public string a { get; set; }
+    public CustomAttribtie(string text)
+    {
+        a = text;
+        Console.WriteLine(a);
+    }
+}
+
+namespace CustomAttributes
+{
+
+    public class Program
+    {
+        [CustomAttribtie("test")]
+        public static void Main(string[] args)
+        {
+            //<summary>
+            // usage of reflection without use
+            //<summary>
+            var testSuites = from t in Assembly.GetExecutingAssembly().GetTypes()
+                             where t.GetCustomAttributes().Any(a => a is CustomAttribtie)
+                             select t;
+
+            typeof(Program).GetCustomAttributes(false);
+          
+        }
+    }
+}
